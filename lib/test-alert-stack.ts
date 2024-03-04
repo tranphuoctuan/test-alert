c3ae3d2bb210e888832c05e6e59cb28c4d8444bd
tranphuoctuan <tranphuoctuan10b2@gmail.com>
2024-03-04 15:56:41 +0700

    v2

---
+++ lib/test-alert-stack.ts
@@ -0,0 +1,33 @@
+import { Stack, StackProps, Duration } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as iam from "aws-cdk-lib/aws-iam";
+
+export class TestAlertStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const provider = new iam.OpenIdConnectProvider(this, "Provider", {
+      url: "https://token.actions.githubusercontent.com",
+      clientIds: ["sts.amazonaws.com"],
+    });
+
+    const role = new iam.Role(this, "DeployRole", {
+      roleName: "DeployRole",
+      maxSessionDuration: Duration.hours(2),
+      assumedBy: new iam.WebIdentityPrincipal(
+        provider.openIdConnectProviderArn,
+        {
+          StringEquals: {
+            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+          },
+          StringLike: {
+            "token.actions.githubusercontent.com:sub": "repo:tranphuoctuan/test-alert:*",
+          },
+        }
+      ),
+    });
+    role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AdministratorAccess")
+    );
+  }
+}
