0329174d530318e3b40bd342f79430e73ac6d1b3
tranphuoctuan <tranphuoctuan10b2@gmail.com>
2024-03-06 15:54:32 +0700

    v20

--- lib/test-alert-stack.ts
+++ lib/test-alert-stack.ts
@@ -1,7 +1,10 @@
-import { Stack, StackProps, Duration } from "aws-cdk-lib";
+import { Stack, StackProps, Duration, CfnOutput } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as iam from "aws-cdk-lib/aws-iam";
-import * as s3 from "aws-cdk-lib/aws-s3"
+import cdk = require('@aws-cdk/core');
+import * as ssm from "aws-cdk-lib/aws-ssm";
+import * as s3 from "aws-cdk-lib/aws-s3";
+
 
 export class TestAlertStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -12,11 +15,47 @@ export class TestAlertStack extends Stack {
       clientIds: ["sts.amazonaws.com"],
     });
 
-    /// create iam user
-    const user = new iam.User(this, 'my_user', {
-      userName: 'UserForPreSignUrl'
+    const s3bucket = new s3.Bucket(this, "S3bucket", {
     })
     
+    const policy = new iam.Policy(this, "policy", {
+
+      policyName: "AmazonS3FullAccess",
+      statements: [
+        new iam.PolicyStatement({
+          actions: ['s3:*'],
+          resources: [s3bucket.bucketArn]
+        })
+      ]
+    })
+
+    /// create iam user
+    const user = new iam.User(this, 'iam_user', {
+      userName: 'User-For-S3PreSignUrl'
+    })
+    const accessKey = new iam.CfnAccessKey(this, 'CfnAccessKey',{
+      userName: user.userName,
+    })
+    policy.attachToUser(user);
+
+  
+
+    // create ssm parameter for AccessKey
+    new ssm.StringParameter(this, 'AccessKeyForUser', {
+      description: "This param is the AccessKey for User Send messages",
+      parameterName: "/Output/User/AccessKey",
+      stringValue: accessKey.ref,
+      tier: ssm.ParameterTier.STANDARD,
+    })
+
+    // Create ssm parameter for SecretAccessKey
+    new ssm.StringParameter(this, 'SecretAccessKeyForUser', {
+      description: "This param is the SecretAccessKey for User Send messages",
+      parameterName: "/Output/User/SecretAccessKey",
+      stringValue: accessKey.attrSecretAccessKey,
+      tier: ssm.ParameterTier.STANDARD,
+    });
+
     const role = new iam.Role(this, "DeployRole", {
       roleName: "DeployRole",
       maxSessionDuration: Duration.hours(2),
